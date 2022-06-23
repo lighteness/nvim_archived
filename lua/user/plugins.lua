64ac7d0b1724f492032cbf9df9295d8a65271baf
Your Name <you@example.com>
2022-06-23 09:04:44 +0800

    [feat] nvim-cmp

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -65,16 +65,16 @@ return packer.startup(function(use)
   use "folke/tokyonight.nvim"
 
   -- cmp plugins
-  --use "hrsh7th/nvim-cmp" -- The completion plugin
-  --use "hrsh7th/cmp-buffer" -- buffer completions
-  --use "hrsh7th/cmp-path" -- path completions
-  --use "hrsh7th/cmp-cmdline" -- cmdline completions
-  --use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  --use "hrsh7th/cmp-nvim-lsp"
---
-  ---- snippets
-  --use "L3MON4D3/LuaSnip" --snippet engine
-  --use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 --
   ---- LSP
   --use "neovim/nvim-lspconfig" -- enable LSP
