5f21e4133f4f3c3173c591f3558ddae38c8bee2f
Your Name <you@example.com>
2022-06-24 17:18:07 +0800

    format

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -16,8 +16,8 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 -- trigger PackerSync right after this file is saved
-vim.api.nvim_create_autocmd({"BufWritePost"},{
-  pattern = {"plugins.lua"},
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = { "plugins.lua" },
   command = "source <afile> | PackerSync",
   once = true
 })
@@ -57,8 +57,8 @@ return packer.startup(function(use)
   --use "lukas-reineke/indent-blankline.nvim"
   --use "goolord/alpha-nvim"
   --use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
-  --use "folke/which-key.nvim"
---
+  use "folke/which-key.nvim"
+  --
   ---- Colorschemes
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
@@ -76,26 +76,26 @@ return packer.startup(function(use)
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
---
+  --
   ---- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   --use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   --use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
---
+  --
   ---- Telescope
   use "nvim-telescope/telescope.nvim"
---
+  --
   -- Treesitter
   use {
-     "nvim-treesitter/nvim-treesitter",
-     run = ":TSUpdate",
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
   }
-   -- use "p00f/nvim-ts-rainbow"
+  -- use "p00f/nvim-ts-rainbow"
   --use "nvim-treesitter/playground"
 
   --use "JoosepAlviste/nvim-ts-context-commentstring"
---
+  --
   ---- Git
   --use "lewis6991/gitsigns.nvim"
 
