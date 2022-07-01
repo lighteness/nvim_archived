853e4d93a3f7a0e46ba79ed3495647517c8e4b52
Sammy Huang <huangliang1203@hotmail.com>
2022-07-01 16:50:33 +0800

    [feat] keymap save current buffer by using alt+s
    .

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -81,5 +81,6 @@ keymap("n", "<F7>", "<cmd>lua require'dap'.step_out()<cr>", opts)
 --keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
 --keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
 
---
+-- Toggle Tagbar
 keymap("n", "<F3>", "<cmd>TagbarToggle<cr>", opts)
+
