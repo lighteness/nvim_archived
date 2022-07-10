c404dc6da54facf1978bab3768729c5771fa2f7d
Sammy Huang <huangliang1203@hotmail.com>
2022-07-10 11:01:41 +0800

    [feat] let telescope to ignore node_modules

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -12,6 +12,9 @@ telescope.setup {
     selection_caret = " ",
     path_display = { "smart" },
 
+    file_ignore_patterns = {"node_modules"},
+
+
     mappings = {
       i = {
         ["<C-n>"] = actions.cycle_history_next,
