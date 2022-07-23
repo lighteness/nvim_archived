d42506b9b0afffa0e3f29b1bfe47f8cfb8ba6208
Sammy Huang <huangliang1203@hotmail.com>
2022-07-23 11:25:22 +0800

    [feat] redefine java refactoring shortcut

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -224,7 +224,7 @@ local mappings = {
     o = { "<Cmd>lua require'jdtls'.organize_imports()<cr>", "organize_imports" },
     v = { "<Cmd>lua require('jdtls').extract_variable()<cr>", "extract_variable" },
     c = { "<Cmd>lua require('jdtls').extract_constant()<cr>", "extract_constant" },
-    m = { "<Esc><Cmd>lua require('jdtls').extract_method(true)<cr>", "extract_method" },
+
     n = { "<Cmd>lua require'jdtls'.test_nearest_method()<cr>", "test_nearest_method" },
     t = { "<Cmd>lua require'jdtls'.test_class()<cr>", "test_class" },
   },
@@ -247,7 +247,13 @@ local mappingsForV = {
     r = { "<Esc><cmd>lua require('refactoring').select_refactor()<cr>", "select_refactor" },
 
 
-  }
+  },
+  j = {
+    name = "Java Refactoring",
+    v = { "<Esc><Cmd>lua require('jdtls').extract_variable(true)<cr>", "extract_variable" },
+    c = { "<Esc><Cmd>lua require('jdtls').extract_constant(true)<cr>", "extract_constant" },
+    m = { "<Esc><Cmd>lua require('jdtls').extract_method(true)<cr>", "extract_method" },
+  },
 }
 local optsForV = {
   mode = "v", -- NORMAL mode
