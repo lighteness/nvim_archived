d53eee521f64f26d07b4d0a34020b536658b3d1d
Sammy Huang <huangliang1203@hotmail.com>
2022-07-27 21:03:32 +0800

    [refactor] redefine shourtcut for dap

--- lua/user/whichkey/common.lua
+++ lua/user/whichkey/common.lua
@@ -18,7 +18,7 @@ function M.debug()
   return {
     d = {
       name = "Debug",
-      b = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Breakpoint" },
+      d = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Breakpoint" },
       c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
       i = { "<cmd>lua require'dap'.step_into()<cr>", "Into" },
       o = { "<cmd>lua require'dap'.step_over()<cr>", "Over" },
@@ -27,6 +27,8 @@ function M.debug()
       l = { "<cmd>lua require'dap'.run_last()<cr>", "Last" },
       u = { "<cmd>lua require'dapui'.toggle()<cr>", "UI" },
       x = { "<cmd>lua require'dap'.terminate()<cr>", "Exit" },
+      C = { "<cmd>lua require'dap'.clear_breakpoints()<cr>", "Clean Breakpoints" },
+      L = { "<cmd>lua require'dap'.list_breakpoints()<cr>", "List Breakpoints" },
     },
   }
 end
