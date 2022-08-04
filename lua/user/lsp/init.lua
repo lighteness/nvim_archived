63a338bc6993e25af1c0ef6cdb0123ba0c1ea500
Sammy Huang <huangliang1203@hotmail.com>
2022-08-04 16:58:45 +0800

    [feat] lsp_signature which helps to see each params in the function

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -2,5 +2,6 @@ require "user.lsp.lsp-installer"
 require "user.lsp.lspconfig".setup()
 require "user.lsp.diagnostic"
 require "user.lsp.null-ls"
+require "user.lsp.lsp_signature"
 
 
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -85,6 +85,7 @@ return packer.startup(function(use)
   --use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
   use "RRethy/vim-illuminate"
+  use "ray-x/lsp_signature.nvim"
 
   --
   ---- Telescope
