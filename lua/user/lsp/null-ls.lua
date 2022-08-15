667d9cd3ed527ea47a5f2911a04edcfdc10d5225
Sammy Huang <huangliang1203@hotmail.com>
2022-08-15 09:58:58 +0800

    [feat] comment out unused formatting plugins

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -15,16 +15,16 @@ local code_actions  = null_ls.builtins.code_actions
 null_ls.setup {
   debug = false,
   sources = {
-    formatting.prettier.with {
-      extra_filetypes = { "toml", "solidity" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-    },
-    formatting.black.with { extra_args = { "--fast" } },
-    formatting.stylua,
-    formatting.shfmt,
+    -- formatting.prettier.with {
+    --   extra_filetypes = { "toml", "solidity" },
+    --   extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    -- },
+    -- formatting.black.with { extra_args = { "--fast" } },
+    -- formatting.stylua,
+    -- formatting.shfmt,
     formatting.google_java_format,
-    diagnostics.flake8,
-    diagnostics.shellcheck,
+    -- diagnostics.flake8,
+    -- diagnostics.shellcheck,
     code_actions.refactoring, --    filetypes = { "go", "javascript", "lua", "python", "typescript" },
 
   },
